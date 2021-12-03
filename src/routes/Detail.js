69eaebba53b6e2b3b3d3bde4c6ae9b196a7e4c7c
unknown <ketty0611@gmail.com>
2021-12-03 17:40:28 +0900

    https://github.com/naturalMin/react-for-beginners.git

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+//url에 있는 변수 값 반환
 
 function Detail() {
-    const {id} = useParams()
-    console.log(id);
+    const {id} = useParams();
+    const getMovie = async () => {
+        const json = await(
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json);
+    }
+    useEffect(() => {
+        getMovie();
+    },[])
+
     return <h1>Detail</h1>;
 }
 export default Detail;
