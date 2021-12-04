9288893af75dda6b6661fb611fdbc49e8f5728e7
ketty <ketty0611@gmail.com>
2021-12-04 15:26:10 +0900

    css commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,14 +8,16 @@ function Home() {
     const [movies, setMovies] = useState([]);
     const getMovies = async() => {
         const json = await (
-        await fetch(
-            "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-        )
+            await fetch(
+                "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+            )
         ).json();    
         setMovies(json.data.movies);
         setLoading(false);
     }
-    useEffect(() => getMovies(), [])
+    useEffect(() => {
+        getMovies();
+    }, [])
     return (       
         <div className = {styles.container}>
             {loading ? (
@@ -31,7 +33,7 @@ function Home() {
                     coverImg = {movie.medium_cover_image}
                     title = {movie.title} 
                     summary = {movie.summary}
-                    genres = {movie.genres}
+                    genres = {movie.genres}                    
                 />)}
                 </div>}
             </div>
